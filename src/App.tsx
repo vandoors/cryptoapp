45caa307dbc4660e94db98d3c92ce87c826ed6e6
vandoors <143453791+vandoors@users.noreply.github.com>
2024-03-15 22:42:17 -0500

    Call the coins api in App.js

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,57 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useEffect } from 'react';
+
+import { get } from 'aws-amplify/api';
+
 import './App.css';
 
-function App() {
+interface Coin {
+  name: string;
+  symbol: string;
+  price_usd: string;
+}
+
+interface CoinsResponse {
+  coins: Coin[];
+}
+
+// Return true if the response is a valid CoinsResponse (TypeScript stuff)
+const isValidCoinsResponse = (jsonResponse: any): jsonResponse is CoinsResponse => {
+  return jsonResponse && jsonResponse.coins && Array.isArray(jsonResponse.coins);
+}
+
+const App = () => {
+  const [coins, updateCoins] = useState<Coin[]>([]);
+
+  const fetchCoins = async () => {
+    const restOperation = await get({
+      apiName: "cryptoapi",
+      path: "/coins"
+    });
+  
+    const { body } = await restOperation.response;
+    const jsonResponse = await body.json();
+  
+    if (isValidCoinsResponse(jsonResponse)) {
+      updateCoins(jsonResponse.coins);
+    } else {
+      console.error("Unexpected response:", jsonResponse);
+    }
+  }
+  
+  useEffect(() => {
+    fetchCoins()
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      {
+        coins.map((coin, index) => (
+          <div key={index}>
+            <h2>{coin.name} - {coin.symbol}</h2>
+            <h5>${coin.price_usd}</h5>
+          </div>
+        ))
+      }
     </div>
   );
 }
