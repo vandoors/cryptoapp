ff3303d2b3ddfb2d29d410de59f3b9c7ad365595
vandoors <143453791+vandoors@users.noreply.github.com>
2024-03-15 22:55:21 -0500

    Update app to support updated coins api

--- src/App.tsx
+++ src/App.tsx
@@ -5,18 +5,37 @@ import { get } from 'aws-amplify/api';
 import './App.css';
 
 interface Coin {
-  name: string;
+  id: string;
   symbol: string;
+  name: string;
+  nameid: string;
+  rank: number;
   price_usd: string;
+  percent_change_24h: string;
+  percent_change_1h: string;
+  percent_change_7d: string;
+  price_btc: string;
+  market_cap_usd: string;
+  volume24: number;
+  volume24a: number;
+  csupply: string;
+  tsupply: string;
+  msupply: string;
+}
+
+interface Info {
+  coins_num: number;
+  time: number;
 }
 
 interface CoinsResponse {
-  coins: Coin[];
+  data: Coin[];
+  info: Info;
 }
 
 // Return true if the response is a valid CoinsResponse (TypeScript stuff)
 const isValidCoinsResponse = (jsonResponse: any): jsonResponse is CoinsResponse => {
-  return jsonResponse && jsonResponse.coins && Array.isArray(jsonResponse.coins);
+  return jsonResponse && jsonResponse.data && Array.isArray(jsonResponse.data);
 }
 
 const App = () => {
@@ -32,7 +51,7 @@ const App = () => {
     const jsonResponse = await body.json();
   
     if (isValidCoinsResponse(jsonResponse)) {
-      updateCoins(jsonResponse.coins);
+      updateCoins(jsonResponse.data);
     } else {
       console.error("Unexpected response:", jsonResponse);
     }
@@ -45,14 +64,18 @@ const App = () => {
   return (
     <div className="App">
       {
-        coins.map((coin, index) => (
-          <div key={index}>
-            <h2>{coin.name} - {coin.symbol}</h2>
-            <h5>${coin.price_usd}</h5>
-          </div>
-        ))
+        coins.length > 0 ? (
+          coins.map((coin, index) => (
+            <div key={index}>
+              <h2>{coin.name} - {coin.symbol}</h2>
+              <h5>${coin.price_usd}</h5>
+            </div>
+          ))
+        ) : (
+          <p>No valid coins data available.</p>
+        )
       }
-    </div>
+  </div>
   );
 }
 
