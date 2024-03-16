1278563d851c052c9e0e584841e63a74672f6754
vandoors <143453791+vandoors@users.noreply.github.com>
2024-03-15 23:21:08 -0500

    Sort coin interface more logically

--- src/App.tsx
+++ src/App.tsx
@@ -5,22 +5,26 @@ import { get } from 'aws-amplify/api';
 import './App.css';
 
 interface Coin {
-  csupply: string;
   id: string;
-  market_cap_usd: string;
-  msupply: string;
   name: string;
   nameid: string;
-  percent_change_1h: string;
-  percent_change_7d: string;
-  percent_change_24h: string;
-  price_btc: string;
-  price_usd: string;
-  rank: number;
   symbol: string;
-  tsupply: string;
+
+  rank: number;
+  market_cap_usd: string;
+  price_usd: string;
+  price_btc: string;
+
+  percent_change_1h: string;
+  percent_change_24h: string;
+  percent_change_7d: string;
+
   volume24: number;
   volume24a: number;
+
+  csupply: string;
+  msupply: string;
+  tsupply: string;
 }
 
 interface CoinsResponse {
