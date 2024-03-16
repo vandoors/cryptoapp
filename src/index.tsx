aa94021e99bd7f974093857d2734f30ecf786307
vandoors <143453791+vandoors@users.noreply.github.com>
2024-03-15 21:33:24 -0500

    Import Amplify + configuration into index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,10 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+import { Amplify } from 'aws-amplify';
+import amplifyConfig from './amplifyconfiguration.json';
+Amplify.configure(amplifyConfig);
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
